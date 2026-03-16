88d94154c725c1be74fac8ac48c846b47929701c
carokann1945 <carokann1945@gmail.com>
2026-03-17 08:51:41 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard', //tailwind와 연동할 css 변수명
+});
+
+export const metadata: Metadata = {
+  title: 'template',
+  description: '',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${pretendard.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
